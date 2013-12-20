34d64f3ba809998240a2c4052aad19b4ae1e3bf5
Matz Tzoe <teamo@live.com>
2013-12-20 11:53:56 +0800

    update package.sh
        now works on "darwin/amd64" and "linux/amd64"

--- lxc/package.sh
+++ lxc/package.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
-# this script build lxc for OS X, Linux amd64, Windows x64, Windows i386, and Linux arm.
-# this script is supposed to run on osx
-
+# this script builds `lxc` for OS X, Linux amd64, Windows x64, Windows i386, and Linux arm.
+# this script works on "darwin/amd64" and "linux/amd64";
 
 # retrieve version
 VER="$(git branch | grep \* | cut -d ' ' -f 2)-$(git show-ref --head | head -1 | fold -w 8 | head -1)"
@@ -11,18 +10,33 @@ if [[ -z $1 ]]; then
   go install -v
 else
   mkdir -p lxc
-  # osx
-  go build -v -o lxc/lxc-osx
+
+  TARGETLIST="darwin/amd64 linux/amd64 windows/amd64 windows/386 linux/arm"
+
+  # native
+  HOSTOS=$(go env GOHOSTOS)
+  HOSTARCH=$(go env GOHOSTARCH)
+  go build -v -o lxc/lxc-${HOSTOS}-${HOSTARCH}
 
   # other platforms
   gocross=$(which gocross.py | cut -d ' ' -f 1)
-  (test ! -z ${gocross}) && (test -f ${gocross}) && (${gocross} win64 build -v -o lxc/lxc-win64.exe)
-  (test ! -z ${gocross}) && (test -f ${gocross}) && (${gocross} win32 build -v -o lxc/lxc-win32.exe)
-  (test ! -z ${gocross}) && (test -f ${gocross}) && (${gocross} linux build -v -o lxc/lxc-linux64)
-  (test ! -z ${gocross}) && (test -f ${gocross}) && (${gocross} arm build -v -o lxc/lxc-arm)
+  for target in ${TARGETLIST};do
+    os=$(echo ${target} | cut -d '/' -f 1)
+    arch=$(echo ${target} | cut -d '/' -f 2)
+    suffix=""
+    if [[ ${os} == "windows" ]];then
+      suffix=".exe"
+    fi
+    if [[ ${os} != ${HOSTOS} || ${arch} != ${HOSTARCH} ]];then
+      (test ! -z ${gocross}) && (test -f ${gocross}) && (${gocross} ${target} build -v -o lxc/lxc-${os}-${arch}${suffix})
+    fi
+  done
 
-  tar czf lxc.bin.tgz lxc
+  tar czf lxc.bin-${VER}.tgz lxc/
   rm -fr lxc/
 fi
 # restore version
 mv conf.go.origin conf.go
+
+echo
+echo -e "\x1b[32mPackage Done\x1b[0m."
